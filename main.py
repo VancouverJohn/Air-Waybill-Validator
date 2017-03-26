e016960bd434c91b16e38a05c9adbd32d6493309
John Smisko <john.smisko@gmail.com>
2017-03-26 15:13:21 -0700

    added more code

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import sys
 
 def main():
 
-    print("Oh eah!!!!")
+    print("Welcome to the Air Waybill Checker.")
+    print("This is meant to help you find the correct number")
+    print("when a number is obscured or missing.\n")
+
+    print("Enter a number between 7 and 8 digits.")
+    print("If 8 digits, enter the column number that is indecipherable (1-8)")
+
+    #userInput = str(sys.argv[1])
+    userInput = str(input('Enter your number:'))
+
+    if len(userInput) < 7 or len(userInput) > 8:
+        print('Please Enter Between 7 or 8 Digits.')
+
+    else:
+        waybillNumber = int(userInput)
+        print(waybillNumber + 2)
 
     pass
 
