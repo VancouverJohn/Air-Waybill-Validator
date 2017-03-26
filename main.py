374fbd04399ed347885129cf2fed40043eed95c2
John Smisko <john.smisko@gmail.com>
2017-03-26 11:38:27 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+def main():
+
+    print("Oh eah!!!!")
+
+    pass
+
+def calculate_possible_numbers(waybill_number, questionable_column):
+
+    pass
+
+def missing_end_column(waybill_number):
+
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
