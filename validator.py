5d497aa79a24a7e540ba0dd07869fb1d972b8a9c
John Smisko <john.smisko@gmail.com>
2017-04-30 12:06:54 -0700

    removed need for termcolor library and cleaned up syntax / help section

--- validator.py
+++ validator.py
@@ -1,15 +1,14 @@
-# Typing 'quit' presents 'Input may only contain spaces and numbers' before closing program
 import sys
-from termcolor import colored
 
-highlightColour = 'red'
+highlightStart ='\x1b[5;31;40m'
+highlightStop = '\x1b[0m'
 
 def main():
 
     userInput = ''
 
     while userInput != 'quit':
-        print("Enter a number between 7 and 8 digits.")
+        print("\nEnter a number between 7 and 8 digits.")
         print('If a column is indecipherable, leave it blank i.e. 12345 78')
         print('If 7 digits are entered, it\'s assumed there is a column missing on the end.')
         userInput = str(input('\nEnter your number, \'quit\', or \'help\':'))
@@ -22,7 +21,7 @@ def main():
 
         if all(x.isdigit() or x.isspace() for x in userInput) == False:
             if userInput != 'quit':
-                print(colored("\nInput may only contain spaces and numbers\n", highlightColour))
+                print(highlightStart + "\nInput may only contain spaces and numbers\n" + highlightStop)
                 continue
             else:
                 break
@@ -40,15 +39,17 @@ def main():
                     missing_column(userInput)
                 else:
                     if check_valid_airwaybill(userInput) == True:
-                        print(colored("\nAir Waybill Number is Valid\n", highlightColour))
+                        print(highlightStart + "\nAir Waybill Number is Valid" + highlightStop)
                     else:
                         calculate_possible_numbers(userInput)
 
 def help():
-    print("\nNote: This program requires Python 3 or above\n")
-    print(colored("Air waybills consist of a 3 digit airline prefix, a 7 digit serial number, and a check digit.", highlightColour))
-    print(colored("This program ignores the prefix.  When you divide the serial number by 7, the remainder should match the check digit.", highlightColour))
-    print(colored("This program tests for a valid serial number by dividing it (the serial number) by 7.  The remainder must match the check digit to be a valid air waybill number.", highlightColour))
+    print("\nNote: This program requires Python 3\n")
+    print("This program checks a given air waybill number for validity.  It only checks the last 8 digits.")
+    print("If an invalid air waybill number is entered, a list of valid potential replacements is displayed.\n")
+    print("The first 3 digits of an air waybill make up the " + highlightStart + "airline prefix" + highlightStop + ".  This is not relevant to validity.")
+    print("The next 7 digits make up the " + highlightStart + "serial number" + highlightStop + ".")
+    print("Dividing the serial number by 7 produces the last (8) digit, called the " + highlightStart + "check digit" + highlightStop + ".")
 
 def check_valid_airwaybill(waybill_number):
     serialNumber = waybill_number[:-1]
@@ -67,10 +68,9 @@ def calculate_possible_numbers(waybill_number):
         second = waybill_number[n+1:]
         for y in range(0, 10):
             y = str(y)
-            #print(first + colored(y, highlightColour) + second)
             number = (first + y + second)
             if check_valid_airwaybill(str(number)) == True:
-                print(first + colored(y, highlightColour) + second)
+                print(first + highlightStart + y + highlightStop + second)
 
 def missing_column(waybill_number):
     first, second = waybill_number.split()
@@ -78,7 +78,7 @@ def missing_column(waybill_number):
         n = str(n)
         testMissingColumn = first + n + second
         if check_valid_airwaybill(testMissingColumn) == True:
-            print(first + colored(n, highlightColour) + second)
+            print(first + highlightStart + n + highlightStop + second)
 
 
 def missing_end_column(waybill_number):
@@ -91,17 +91,17 @@ def missing_end_column(waybill_number):
         mod = int(testFirstSeven % 7)
         if mod == checkDigit:
             if n == 0:
-                print(colored('0', highlightColour) + waybill_number)
+                print(highlightStart + '0' + highlightStop + waybill_number)
             else:
-                print(colored(str(n), highlightColour) + waybill_number)
+                print(highlightStart + str(n) + highlightStop + waybill_number)
     firstSeven = int(waybill_number)
     remainder = firstSeven % 7
     firstDigit = int(waybill_number[:1])
     if firstDigit == 0:
         placeHolder = waybill_number[1:]
-        print('0' + placeHolder + colored(str(remainder), highlightColour))
+        print('0' + placeHolder + highlightStart + str(remainder) + highlightStop)
     else:
-        print(str(firstSeven) + colored(str(remainder), highlightColour))
+        print(str(firstSeven) + highlightStart + str(remainder) + highlightStop)
 
 if __name__ == '__main__':
     sys.exit(main())
